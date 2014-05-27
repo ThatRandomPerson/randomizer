96706acaee1587b3e9b1391a6f649f1800ed8b89
ThatRandomPerson <eric.corbu@gmail.com>
2014-05-27 17:45:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+var firstnum = document.getElementById("firstnum");
+var secondnum = document.getElementById("secondnum");
+var num = document.getElementById('num').innerHTML = Math.floor(Math.random() * "secondnum") + "firstnum";
