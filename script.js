d59a4a833ab3e2b83b9b262327a93ef399b25d94
ThatRandomPerson <eric.corbu@gmail.com>
2014-05-27 20:14:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var firstnum = document.getElementById("firstnum").value;
 var secondnum = document.getElementById("secondnum").value;
-var num = Math.floor(Math.random() * "secondnum") + "firstnum";
+var num = Math.floor(Math.random() * secondnum) + firstnum;
 document.getElementById('answer').innerHTML= num;
